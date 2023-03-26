bf76c23f424f85a61eb4d9cdaf44bd5e0ac84d80
Vamsi Krishna <you@example.com>
2023-03-26 15:43:41 +0530

    Modified the file

--- python.py/findprime.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Enter a number: "))
-y = int(input("Enter second number: "))
-for num in range(x, y+1):
-    if num>1:
-        for i in range(2, num):
-            if num%i == 0:
-                break
-        else:
-            print(num)
